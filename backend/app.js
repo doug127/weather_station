13ba50da6f06a56b66844a0eaa59da13fd4d8d57
Menas- <menatlfn@gmail.com>
2025-07-24 02:04:07 -0400

    cambios

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(cors({
-<<<<<<< HEAD
-  origin: 'http://localhost:5173', 
-  credentials: true,
-=======
   origin: 'http://localhost:5173',
-  credentials: true
->>>>>>> 9c59a07bf16529c233edbb7fd36a15211024d0c7
+  credentials: true,
 }));
 app.use(express.json());
 
