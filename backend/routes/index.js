31e9b657746c398acd22477881480a0c2f8e44a6
doug127 <doutarazona09@gmail.com>
2025-07-27 19:32:25 -0400

    Mejora al predecir modelo, creacion de controller para generar reporte excel

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,17 @@ import express from 'express'
 import sensorRouter from './sensor.routes.js';
 import valueRouter from './value.routes.js';
 import variableRouter from './variable.routes.js'
+import momentRouter from './moments.routes.js'
 import trainModelRouter from './trainModel.routes.js'
+import reportRouter from './reports.routes.js'
 
 const routes = express.Router();
 
 routes.use('/sensor', sensorRouter);
 routes.use('/value', valueRouter);
 routes.use('/variable', variableRouter);
+routes.use('/moment', momentRouter);
 routes.use('/model', trainModelRouter);
+routes.use('/report', reportRouter);
 
 export default routes;
